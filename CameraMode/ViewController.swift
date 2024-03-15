3bc41e4b74dc43db6681e4b0d96f416b81000c65
Elaidzha1940 <eli080293@gmail.com>
2024-03-15 20:01:34 +0300

    CameraMode

---
+++ CameraMode/ViewController.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: CameraMode
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.03.2024
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+    }
+}
+
