7403a5eb8d039b404b0c1c471acc967210149679
Elaidzha1940 <eli080293@gmail.com>
2024-03-16 12:33:47 +0300

    Done

--- CameraMode/Camera/ViewController.swift
+++ CameraMode/Camera/ViewController.swift
@@ -32,13 +32,29 @@ class ViewController: UIViewController {
     }
     
     private func configureFlashButton() {
+        guard let button = flashButton else {
+            return
+        }
         let systemName = viewModel.isFlashEnabled ? "flashlight.off.circle.fill" : "flashlight.slash.circle"
-        flashButton.setImage(UIImage(systemName: systemName), for: .normal)
+        button.setImage(UIImage(systemName: systemName), for: .normal)
     }
     
+    //    private func configureFlashButton() {
+    //        let systemName = viewModel.isFlashEnabled ? "flashlight.on.circle" : "flashlight.slash.circle.fill"
+    //        flashButton.setImage(UIImage(systemName: systemName), for: .normal)
+    //    }
+    
     private func configureLivePhotoButton() {
+        guard let button = livePhotoButton else {
+            return
+        }
         let systemName = viewModel.isFlashEnabled ? "livephoto" : "livephoto.slash"
-        livePhotoButton.setImage(UIImage(systemName: systemName), for: .normal)
+        button.setImage(UIImage(systemName: systemName), for: .normal)
     }
+    
+    //    private func configureLivePhotoButton() {
+    //        let systemName = viewModel.isFlashEnabled ? "livephoto" : "livephoto.slash"
+    //        livePhotoButton.setImage(UIImage(systemName: systemName), for: .normal)
+    //    }
 }
 
