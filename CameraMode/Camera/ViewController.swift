293080151c01f92d1be9849a3ccf598c14b9065e
Elaidzha1940 <eli080293@gmail.com>
2024-03-16 11:36:54 +0300

    Updtae ViewController

--- CameraMode/Camera/ViewController.swift
+++ CameraMode/Camera/ViewController.swift
@@ -10,10 +10,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var flashButton: UIButton!
+    @IBOutlet private var livePhotoButton: UIButton!
+    
+    let viewModel: CameraViewModelProtocol = CameraViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        configureFlashButton()
+    }
+    
+    private func configureFlashButton() {
+        let systemName = viewModel.isFlashEnabled ? "lightbulb" : "lightbulb.slash"
+        flashButton.setImage(UIImage(systemName: systemName), for: .normal)
+    }
+    
+    private func configureLivePhotoButton() {
+        let systemName = viewModel.isFlashEnabled ? "livephoto" : "livephoto.slash"
+        flashButton.setImage(UIImage(systemName: systemName), for: .normal)
     }
 }
 
